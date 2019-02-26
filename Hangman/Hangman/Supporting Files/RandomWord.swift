4a138794ccc729e41327be936294fed196e10cd3
Sophia Song <sophies1234@gmail.com>
2019-02-26 13:30:51 -0800

    First commit

---
+++ Hangman/Hangman/Supporting Files/RandomWord.swift
@@ -0,0 +1,16 @@
+//
+//  RandomWord.swift
+//  Hangman
+//
+//  Created by Sophia Song on 2/26/19.
+//  Copyright © 2019 iosdecal. All rights reserved.
+//
+
+import Foundation;
+
+
+
+
+let wordBank = ["awkward", "bagpipes", "banjo", "bungler", "croquet", "crypt", "dwarves", "fervid", "fishhook", "fjord", "gazebo", "gypsy", "haiku", "haphazard", "hyphen", "ivory", "jazzy", "jiffy", "jinx", "jukebox", "kayak", "kiosk", "klutz", "memento", "mystify", "numbskull", "ostracize", "oxygen", "pajama", "phlegm", "pixel", "polka", "quad",  "quip" ,"rhythmic" ,"rogue" ,"sphinx","squawk" ,"swivel" ,"toady" ,"twelfth" ,"unzip" ,"waxy" ,"wildebeest" ,"yacht" ,"zealous" ,"zigzag" ,"zippy", "zombie"]
+
+
